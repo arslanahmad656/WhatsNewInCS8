ee5dd447da32b5d1d6cc359c764c8759f4cec224
Arslan Ahmad <ars.ahm6@gmail.com>
2019-12-17 18:04:28 +0500

    Project for .NET core added to demonstrate the features available only in .Net Standard 2.1

---
+++ Main Solution/WhatsNewInCS8Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WhatsNewInCS8Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
